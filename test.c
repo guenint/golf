e64ae1fe89d422780b0b5e0c484741ba0a1e5f40
guenint <tedguenin@gmail.com>
2014-10-25 09:50:31 -0400

    ADC stuff part 1

--- test.c
+++ test.c
@@ -1,15 +1,27 @@
 /*psuedocode
 */
 
+//dependencies
+#include <avr/io.h>
+#include "m_general.h"
+#include <avr/interrupt.h>
+#define TIMER_MAX 0x80;
+
 //prototype functions
 turn(char direction, int time);
 fire(int power);
-
+init(void);
+enable_timer(void);
+analog_input(void);
 //enable ADC functionality
 
-int main(void){
-    //take in ADC values
+volatile int OCR0A = TIMER_MAX;
+volatile int OCR0B = 0x00;
 
+
+int main(void){
+    //take in ADC values and initialize timer
+    init();
     //lots of if statements...
     //call turn() based on ADC values
     //threshold to prevent shaking around target
@@ -18,6 +30,75 @@ int main(void){
     
     //fire that shit
 }
+
+void init(void)
+{
+    // Sets B6
+    set(DDRB, 6);
+    //runs method analog_input below
+    analog_input();
+    //runs method enable_timer below
+    enable_timer();
+}
+
+void analog_input(void)
+{
+    // hardware settings
+    m_disableJTAG();
+    m_clockdivide(0);
+
+    //voltage reference - set to Vcc
+    clear(ADMUX, REFS1);
+    set(ADMUX, REFS0);
+
+    // Scale to /128, for 125kHz
+    set(ADCSRA, ADPS2);
+    set(ADCSRA, ADPS1);
+    set(ADCSRA, ADPS0);
+
+    //disable digital input bits 0-7
+    set(DIDR0, ADC0D);
+
+    //enables interrupts
+    clear(ADCSRA, ADIE);
+
+    // Disable ADC system
+    clear(ADCSRA, ADEN);
+    
+    //F0 input for timer
+    clear(ADCSRB, MUX5);
+    clear(ADMUX, MUX2);
+    clear(ADMUX, MUX1);
+    clear(ADMUX, MUX0);
+
+    // Enable ADC system
+    set(ADCSRA, ADEN);
+
+    //Begins the conversion
+    set(ADCSRA, ADSC);
+}
+
+void enable_timer(void)
+{
+    // timer values
+    OCR0A = TIMER_MAX;
+    OCR0B = 0x00;
+
+    // Sets the timer mode: OCR0A register
+    set(TCCR0B, WGM02);
+    set(TCCR0A, WGM01);
+    set(TCCR0A, WGM00);
+
+    //compare B7
+    set(TCCR0A, COM0B1);
+    clear(TCCR0A, COM0B0);
+
+    // clockdivide
+    set(TCCR0B, CS02);
+    clear(TCCR0B, CS01);
+    set(TCCR0B, CS00);
+}
+
 
 //motor turn function
 //better functionality would be to turn for degrees not time... need feedback loop for this probably. worth it?
